8a6fe42ed076e0ed6e10322e8390fb51b6ec8098
jason laferrera <jason.laferrera@gmail.com>
2014-01-28 16:41:33 -0500

    added goodbyesucker

--- hello_world.rb
+++ hello_world.rb
@@ -1,3 +1,13 @@
 require 'sinatra'
-get '/hello' do "Hello World!" end
-get '/:name' do "Hello #{params[:name]}!" end
+
+get '/hello' do 
+  "Hello World!" 
+end
+
+get '/:name' do 
+  "Hello #{params[:name]}!" 
+end
+
+get '/goodbye' do
+  "Seeya SUCKER!"
+end
